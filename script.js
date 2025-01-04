9b850df4d6b8e60d9fd0c65fee5a5023ebcf524a
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-05 01:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 //your JS code here. If required.
-const circles = document.querySelector(".circle");
+const circles = Arrayfrom(document.querySelectorAll(".circle"));
 const nextButton = document.getElementById("next");
 const prevButton = document.getElementById("prev");
 
-let activeCircle = 1;
+let activeCircle = 0;
 userInterface();
 
 nextButton.addEventListener('click',() => {
 	activeCircle++;
-	if(activeCircle > circles.length){
-		activeCircle = circles.length;
-	}
+	// if(activeCircle > circles.length){
+	// 	activeCircle = circles.length;
+	// }
 	userInterface();
 });
 
 prevButton.addEventListener('click', () =>{
 	activeCircle--;
-	if(activeCircle < 1){
-		activeCircle = 1;
-	}
+	// if(activeCircle < 1){
+	// 	activeCircle = 1;
+	// }
 	userInterface();
 });
 
 function userInterface() {
 	circles.forEach((circle, index) => {
-		if(circle < activeCircle){
-			circles.classList.add('active');
+		if(index <= activeCircle){
+			circle.classList.add('active');
 		} else {
-			circles.classList.remove('active');
+			circle.classList.remove('active');
 		}
 	});
-	prevButton.disabled = activeCircle ===1;
-	nextButton.disabled = activeCircle === circles.length;
+	prevButton.disabled = activeCircle ===0;
+	nextButton.disabled = activeCircle === circles.length-;
 }
